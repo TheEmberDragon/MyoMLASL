4df5cc817faa97752e3fcb0e00ffb528eea93351
TheEmberDragon <theemberdragon@gmail.com>
2018-06-17 15:53:52 -0500

    Made script work with multiple csv trials. Excluded csv in ignored.

--- csvCombiner.py
+++ csvCombiner.py
@@ -1,38 +1,56 @@
+import fnmatch
 import numpy as np
+import os
 import pandas as pd
+import re
 from functools import reduce
+from tkinter import Tk
+from tkinter import filedialog
 
-dataID = "-1527282613.csv"
+root = Tk()
+root.withdraw()
 
-#Get the data frames from the CSV files
-dataFrameAccel = pd.read_csv("accelerometer" + dataID)
-dataFrameEmg = pd.read_csv("emg" + dataID)
-dataFrameGyro = pd.read_csv("gyro" + dataID)
-dataFrameOrientation = pd.read_csv("orientation" + dataID)
-dataFrameEuler = pd.read_csv("orientationEuler" + dataID)
+# Get the directory containing the csv files
+curDirectory = filedialog.askdirectory()
 
-#print(dataFrameAccel)
-#print(dataFrameEmg)
-#print(dataFrameGyro)
-#print(dataFrameOrientation)
-#print(dataFrameEuler)
+# Get a list of ids that are being used for the csv files
+dataIDList = []
+for file in os.listdir(curDirectory):
+    if fnmatch.fnmatch(file, 'emg*.csv'):
+        dataIDList.append((re.split('-',file))[1])
 
-#Get a list of all the data frames needed
-dataFrames = [dataFrameAccel,dataFrameEmg,dataFrameGyro,dataFrameOrientation,dataFrameEuler]
+# Loop through the ids and combine the respective csv files
+for dataID in dataIDList:
+    print(dataID)
+    # Get the data frames from the CSV files
+    dataFrameAccel = pd.read_csv(curDirectory + "/accelerometer-" + dataID)
+    dataFrameEmg = pd.read_csv(curDirectory + "/emg-" + dataID)
+    dataFrameGyro = pd.read_csv(curDirectory + "/gyro-" + dataID)
+    dataFrameOrientation = pd.read_csv(curDirectory + "/orientation-" + dataID)
+    dataFrameEuler = pd.read_csv(curDirectory + "/orientationEuler-" + dataID)
 
-#Combine the data frames on the timestamp column
-dataFrameResult = reduce(lambda left,right: pd.merge(left, right, on='timestamp', how='outer'), dataFrames)
+    # print(dataFrameAccel)
+    # print(dataFrameEmg)
+    # print(dataFrameGyro)
+    # print(dataFrameOrientation)
+    # print(dataFrameEuler)
 
-#Sort the data frame by the timestamp value
-dataFrameResult = dataFrameResult.sort_values(by=['timestamp'])
+    # Get a list of all the data frames needed
+    dataFrames = [dataFrameAccel, dataFrameEmg, dataFrameGyro, dataFrameOrientation, dataFrameEuler]
 
-#Fill blank values forward (taking previous value) whenever possible
-dataFrameResult = dataFrameResult.fillna(method='pad')
+    # Combine the data frames on the timestamp column
+    dataFrameResult = reduce(lambda left, right: pd.merge(left, right, on='timestamp', how='outer'), dataFrames)
 
-#Fill the other blank values with back propagation
-dataFrameResult = dataFrameResult.fillna(method='bfill')
+    # Sort the data frame by the timestamp value
+    dataFrameResult = dataFrameResult.sort_values(by=['timestamp'])
 
-#print(dataFrameResult)
+    # Fill blank values forward (taking previous value) whenever possible
+    dataFrameResult = dataFrameResult.fillna(method='pad')
 
-#Write the data frame result to a CSV
-dataFrameResult.to_csv("result" + dataID)
+    # Fill the other blank values with zeros
+    dataFrameResult = dataFrameResult.fillna(0)
+
+    # print(dataFrameResult)
+
+    # Write the data frame result to a CSV
+    dataFrameResult.to_csv("result-" + dataID)
